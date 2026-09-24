637279cb62b0fec59e0b618d5b925ac3e20d419b
lasersquad0 <lasersquad@gmail.com>
2026-09-24 18:04:54 +0300

    .

--- external/utils/src/string_utils.cpp
+++ external/utils/src/string_utils.cpp
@@ -1,5 +1,5 @@
 
-#include <Windows.h>
+#include <windows.h>
 #include <format>
 #include <stdexcept>
 #include <cassert>
@@ -8,8 +8,8 @@
 
 
 // special non-template function for wchar_t*
-template<>
-std::string wtos<wchar_t*>(const pwchar_t& wstr)
+//template<>
+std::string wtos(const wchar_t* wstr)
 {
     std::wstring wwstr(wstr);
     int len = WideCharToMultiByte(CP_UTF8, 0 /*WC_NO_BEST_FIT_CHARS*/, wwstr.data(), (int)wwstr.length(), nullptr, 0, nullptr, nullptr);
@@ -17,8 +17,8 @@ std::string wtos<wchar_t*>(const pwchar_t& wstr)
     if (len == 0)
         throw std::runtime_error(std::format("Error in WideCharToMultiByte 1. Error code: {}", GetLastError()));
 
-    std::string dest;
-    dest.resize(len);
+    std::string dest(len, 0);
+    //dest.resize(len);
     int err = WideCharToMultiByte(CP_UTF8, 0 /*WC_NO_BEST_FIT_CHARS*/, wwstr.data(), (int)wwstr.size(), dest.data(), len, nullptr, nullptr);
 
     if (!err)
@@ -27,23 +27,25 @@ std::string wtos<wchar_t*>(const pwchar_t& wstr)
     return dest;
 }
 
-std::wstring stow(const std::string& str)
+// special non-template function for char*
+//template<>
+std::wstring stow(const char* astr)
 {
-    int bufferSize = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, nullptr, 0); // Get the required buffer size
+    int bufferSize = MultiByteToWideChar(CP_UTF8, 0, astr, -1, nullptr, 0); // Get the required buffer size
     if (bufferSize == 0)
-        throw std::runtime_error(std::format("Error in WideCharToMultiByte 1. Error code: {}", GetLastError()));
+        throw std::runtime_error(std::format("Error in MultiByteToWideChar 1. Error code: {}", GetLastError()));
 
     std::wstring wstr(bufferSize, 0);
 
-    int result = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, &wstr[0], bufferSize);
+    int result = MultiByteToWideChar(CP_UTF8, 0, astr, -1, &wstr[0], bufferSize);
     if (result == 0)
-        throw std::runtime_error(std::format("Error in WideCharToMultiByte 2. Error code: {}", GetLastError()));
+        throw std::runtime_error(std::format("Error in MultiByteToWideChar 2. Error code: {}", GetLastError()));
 
     wstr.resize(bufferSize - 1);
     return wstr;
 }
 
-void Trim(std::string& str) //Note: str will be changed
+static void Trim(std::string& str) //Note: str will be changed
 {
     // remove any leading and traling spaces and tabs.
     size_t strBegin = str.find_first_not_of(" \t");
